69f3bbc272c580c7e67cb360c3a37d17f48befe9
abina16 <113548216+abina16@users.noreply.github.com>
2022-11-02 10:58:07 +0530

    added another pattern

--- recursion_substring.java
+++ recursion_substring.java
@@ -15,7 +15,7 @@ public class programming {
 		}
 		ans.add(arr.get(ind));
 		sub(ind+1,ans,arr,n);
-		ans.remove(ans.remove(ans.size()-1));
+		ans.remove(ans.size()-1);
 		sub(ind+1,ans,arr,n);
 	}
 	public static void main(String args[]) throws java.lang.Exception {
@@ -75,5 +75,49 @@ public class programming {
 		int n =3;
 		int sum = 2;
 		sub(0,ans,0,sum,arr,n);
+	}
+}
+
+
+// To count all the substring of an array which has a sum of 2
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class programming {
+	static int  count(ArrayList <Integer> arr,int ind,int n,int sum,int s){
+		if (ind==n) {
+			if (sum==s) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		s+=arr.get(ind);
+		int l = count (arr,ind+1,n,sum,s);
+		
+		s-=arr.get(ind);
+		int r = count (arr,ind+1,n,sum,s);
+		
+		return l+r;
+	}
+	
+	public static void main(String args[]) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+			ArrayList <Integer> arr = new ArrayList<Integer>();
+			arr.add(1);
+			arr.add(2);
+			arr.add(1);
+			int ind = 0;
+			int n = arr.size();
+			int sum=2;
+			int s =0;
+			
+			System.out.println(count(arr,ind,n,sum,s));
+		}
+		sc.close();
+		
 	}
 }
